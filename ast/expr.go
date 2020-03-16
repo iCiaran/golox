473f0b7216194aa488830ead7db8220801722fea
iCiaran <iCiaran@users.noreply.github.com>
2020-03-16 22:48:38 +0000

    Add ast generation tool
     + generated expr.go file

---
+++ ast/expr.go
@@ -0,0 +1,61 @@
+package ast
+
+import (
+	"github.com/iCiaran/golox/token"
+)
+
+type Visitor interface {
+	VisitBinaryExpr(expr *Binary) interface{}
+	VisitGroupingExpr(expr *Grouping) interface{}
+	VisitLiteralExpr(expr *Literal) interface{}
+	VisitUnaryExpr(expr *Unary) interface{}
+}
+type Expr interface {
+	Accept(v Visitor) interface{}
+}
+type Binary struct {
+	Left     Expr
+	Operator *token.Token
+	Right    Expr
+}
+
+func NewBinary(left Expr, operator *token.Token, right Expr) *Binary {
+	return &Binary{Left: left, Operator: operator, Right: right}
+}
+func (b *Binary) Accept(v Visitor) interface{} {
+	return v.VisitBinaryExpr(b)
+}
+
+type Grouping struct {
+	Expression Expr
+}
+
+func NewGrouping(expression Expr) *Grouping {
+	return &Grouping{Expression: expression}
+}
+func (g *Grouping) Accept(v Visitor) interface{} {
+	return v.VisitGroupingExpr(g)
+}
+
+type Literal struct {
+	Value interface{}
+}
+
+func NewLiteral(value interface{}) *Literal {
+	return &Literal{Value: value}
+}
+func (l *Literal) Accept(v Visitor) interface{} {
+	return v.VisitLiteralExpr(l)
+}
+
+type Unary struct {
+	Operator *token.Token
+	Right    Expr
+}
+
+func NewUnary(operator *token.Token, right Expr) *Unary {
+	return &Unary{Operator: operator, Right: right}
+}
+func (u *Unary) Accept(v Visitor) interface{} {
+	return v.VisitUnaryExpr(u)
+}
